dd9e4d5209aa7cfd49536ff72be3236c6c546ddc
Stephen Cai <KK201431873@gmail.com>
2025-11-03 01:15:40 -0500

    cleared arduino_serial_thread.py functions

--- threads/arduino_serial/arduino_serial_thread.py
+++ threads/arduino_serial/arduino_serial_thread.py
@@ -3,23 +3,15 @@ from utils import PiThread
 import time
 
 class ArduinoSerialThread(PiThread):
-    print_toggle: bool = True
-    last_time: float = -1
-
     def _on_created_impl(self) -> None:
-        self["print"] = self.print_toggle
+        pass
 
     def _on_start_impl(self) -> None:
-        self.last_time = time.perf_counter()
-        self.print("Alive!")
+        pass
 
 
     def _loop_impl(self) -> None:
-        now = time.perf_counter()
-        if now - self.last_time >= 1.0:
-            self.print_toggle = not self.print_toggle
-            self["print"] = self.print_toggle
-            self.last_time = now
+        pass
     
 
     def _on_shutdown_impl(self) -> None:
