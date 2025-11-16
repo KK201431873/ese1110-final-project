f968e292f1b770386dc6a6a4f61004e183a79b39
Stephen Cai <KK201431873@gmail.com>
2025-11-15 20:30:57 -0500

    change bounds for set_intake_position

--- threads/controller/controller_serial_interface.py
+++ threads/controller/controller_serial_interface.py
@@ -2,7 +2,7 @@ from utils.mcu_serial_interface import MCUSerialInterface
 
 # --- Controller functions ---
 def drive_speed_angle(speed: float, angle: float) -> None:
-    """Positive speed drives forward, positive angle turns CCW. Speed and angle must be between -1.0 and 1.0."""
+    """Positive speed drives forward, positive angle turns CCW. Speed and angle must both be between `-1.0` and `1.0`."""
     speed = clamp(speed, -1.0, 1.0)
     angle = clamp(angle, -1.0, 1.0)
     left = clamp(speed - angle, -1.0, 1.0)
@@ -11,12 +11,12 @@ def drive_speed_angle(speed: float, angle: float) -> None:
     set_right_drive_power(right)
 
 def set_left_drive_power(power: float) -> None:
-    """Set the power of the left drive motor. Power must be between -1.0 and 1.0."""
+    """Set the power of the left drive motor. Power must be between `-1.0` and `1.0`."""
     power = clamp(power, -1.0, 1.0)
     MCUSerialInterface.write_line("ControllerThread", f"command.drive.left:{power}")
 
 def set_right_drive_power(power: float) -> None:
-    """Set the power of the right drive motor. Power must be between -1.0 and 1.0."""
+    """Set the power of the right drive motor. Power must be between `-1.0` and `1.0`."""
     power = clamp(power, -1.0, 1.0)
     MCUSerialInterface.write_line("ControllerThread", f"command.drive.right:{power}")
 
@@ -25,13 +25,13 @@ def stop_drive() -> None:
     set_left_drive_power(0.0)
     set_right_drive_power(0.0)
 
-def set_intake_position(degrees: int) -> None:
-    """Set the position (in degrees) of the intake servo. Position must be between 0 and 180."""
-    degrees = clamp(degrees, 0, 180)
+def set_intake_position(degrees: float) -> None:
+    """Set the position of the intake servo. Position must be between `0.0` and `1.0`."""
+    degrees = clamp(degrees, 0.0, 1.0)
     MCUSerialInterface.write_line("ControllerThread", f"command.intake.servo:{degrees}")
 
 def set_intake_power(power: float) -> None:
-    """Set the power of the intake motor. Power must be between -1.0 and 1.0."""
+    """Set the power of the intake motor. Power must be between `-1.0` and `1.0`."""
     power = clamp(power, -1.0, 1.0)
     MCUSerialInterface.write_line("ControllerThread", f"command.intake.motor:{power}")
 
