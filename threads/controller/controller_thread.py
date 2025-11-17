a32ce898603aa03cd872e9e0d480757f59fbe6dd
Stephen Cai <KK201431873@gmail.com>
2025-11-16 22:34:46 -0500

    implemented transfer logic

--- threads/controller/controller_thread.py
+++ threads/controller/controller_thread.py
@@ -19,6 +19,10 @@ def normalize_angle(radians: float) -> float:
         radians += 2 * math.pi
     return radians
 
+def clamp(x: float, lower: float, upper: float) -> float:
+    """Clamp the given value `x` between [`lower`, `upper`]."""
+    return max(lower, min(upper, x))
+
 class State(Enum):
     SEARCH = 0
     PICKUP = 1
@@ -43,6 +47,9 @@ class ControllerThread(PiThread):
     MAX_PICKUP_SPEED: float
     """Proportion of maximum linear velocity to cap speed at during PICKUP."""
 
+    SPEED_kP: float
+    """Coefficient multiplied to distance from target ball to get speed."""
+
     kP: float
     """Proportional coefficient of PID controller."""
     kI: float
@@ -52,6 +59,12 @@ class ControllerThread(PiThread):
     _pid_controller: PIDController | None
     """Instance of a PID controller."""
 
+    BALL_PICKUP_THRESHOLD: float
+    """TEMPORARY VARIABLE (we don't have ball sensor) Distance threshold to assume we picked up a ball and change to TRANSFER state (meters)."""
+
+    TRANSFER_DURATION: float
+    """Amount of time to remain in the TRANSFER state, must be long enough for ball to fall into bin (seconds)."""
+
     _ball_points: tuple[Vector2, Vector2] | None
     """(Relative, Absolute) points of the actively targeted ball."""
 
@@ -60,6 +73,9 @@ class ControllerThread(PiThread):
 
     _last_retarget_time: float
     """Timestamp of the last change in the target position."""
+
+    _transfer_start_time: float
+    """Time when the robot switches to TRANSFER state."""
 
     def _on_created_impl(self) -> None:
         self.STATE = State.SEARCH
@@ -72,13 +88,17 @@ class ControllerThread(PiThread):
         self.BALL_MAX_DRIFT = controller_settings["BALL_MAX_DRIFT"]
         self.HEADING_PRIORITY = controller_settings["HEADING_PRIORITY"]
         self.MAX_PICKUP_SPEED = controller_settings["MAX_PICKUP_SPEED"]
+        self.SPEED_kP = controller_settings["SPEED_kP"]
         self.kP = controller_settings["kP"]
         self.kI = controller_settings["kI"]
         self.kD = controller_settings["kD"]
+        self.BALL_PICKUP_THRESHOLD = controller_settings["BALL_PICKUP_THRESHOLD"] #TODO: remove when we get ball sensor
+        self.TRANSFER_DURATION = controller_settings["TRANSFER_DURATION"]
 
         self._ball_points = None
         self._last_detection_time = 0.0
         self._last_retarget_time = 0.0
+        self._transfer_start_time = 0.0
 
         self._pid_controller = None
 
@@ -102,12 +122,12 @@ class ControllerThread(PiThread):
             # --- The robot is searching for a ping-pong ball ---
             case State.SEARCH:
                 # Turn counterclockwise until found ball
-                # controller.set_left_drive_power(-0.25)
-                # controller.set_right_drive_power(0.25)
+                controller.set_left_drive_power(-0.25)
+                controller.set_right_drive_power(0.25)
 
                 # Raise and stop intake
-                # controller.set_intake_position(0)
-                # controller.set_intake_power(0.0)
+                controller.set_intake_position(0)
+                controller.set_intake_power(0.0)
                 
                 # Go PICKUP if a ball is detected
                 self._ball_points = self.get_closest_ball_points()
@@ -126,7 +146,7 @@ class ControllerThread(PiThread):
                 robot_pose: Pose2 | None = SensorThread["localization.pose"]
 
                 # Handle ball tracking and fallback to SEARCH
-                change_state, ball_detected = self.track_ball(now)
+                change_state = self.track_ball(now)
                 if change_state or robot_pose is None or self._ball_points is None or self._pid_controller is None:
                     self._ball_points = None
                     self.STATE = State.SEARCH
@@ -136,43 +156,51 @@ class ControllerThread(PiThread):
                 controller.set_intake_position(0.15)
                 controller.set_intake_power(1.0)
                 
-                if ball_detected:
-                    # Control heading and approach ball (convert absolute to relative coordinates using robot pose)
-                    dx = self._ball_points[1].x - robot_pose.COORDS.x
-                    dy = self._ball_points[1].y - robot_pose.COORDS.y
-                    cos = math.cos(robot_pose.h)
-                    sin = math.sin(robot_pose.h)
+                # Control heading and approach ball (convert absolute to relative coordinates using robot pose)
+                dx = self._ball_points[1].x - robot_pose.COORDS.x
+                dy = self._ball_points[1].y - robot_pose.COORDS.y
+                cos = math.cos(robot_pose.h)
+                sin = math.sin(robot_pose.h)
 
-                    # Rotate displacement vector to get error
-                    e_x =  dx*cos + dy*sin
-                    e_y = -dx*sin + dy*cos
+                # Rotate displacement vector to get error
+                e_x =  dx*cos + dy*sin
+                e_y = -dx*sin + dy*cos
 
-                    if abs(e_x) < 1e-3:
-                        e_x = 1e-3 # very, very rare edge case
-                    e_h = normalize_angle(math.atan2(e_y, e_x)) # Heading error (rad)
-                    k = math.exp(-e_h*e_h*self.HEADING_PRIORITY)
+                if abs(e_x) < 1e-3:
+                    e_x = 1e-3 # very, very rare edge case
+                e_h = normalize_angle(math.atan2(e_y, e_x)) # Heading error (rad)
+                k = math.exp(-e_h*e_h*self.HEADING_PRIORITY)
 
-                    # Control outputs
-                    speed = k*self.MAX_PICKUP_SPEED
-                    angle = self._pid_controller.update(e_h)
-                else:
-                    # Don't drive if no ball detected
-                    speed = 0
-                    angle = 0
+                # Get distance to ball
+                distance = math.hypot(e_x, e_y)
+
+                # Control outputs
+                speed = clamp(0, self.SPEED_kP * distance * k, self.MAX_PICKUP_SPEED)
+                angle = self._pid_controller.update(e_h)
                 
                 controller.drive_speed_angle(speed, angle)
                 WebSocketInterface.send_variable(self, "control.output.speed", str(speed))
                 WebSocketInterface.send_variable(self, "control.output.angle", str(angle))
 
+                # State change logic
+                if distance < self.BALL_PICKUP_THRESHOLD:
+                    self._transfer_start_time = now
+                    self.STATE = State.TRANSFER
+                    return
+                
+
             # --- The robot is transferring the ball to its bin ---
             case State.TRANSFER:
-                
-                # TODO: Implement transfer logic
-                pass
+                # Stop moving, raise intake, keep running rollers
+                controller.stop_drive()
+                controller.set_intake_position(0)
+                controller.set_intake_power(1.0)
 
-            
-            case _: # This should never happen
-                pass
+                # State change logic
+                if now - self._transfer_start_time > self.TRANSFER_DURATION:
+                    self.STATE = State.SEARCH
+                    return
+                
   
         # Share target data for minimap
         self["target_ball_points"] = self._ball_points # (Relative, Absolute) points of actively targeted ball
@@ -198,7 +226,7 @@ class ControllerThread(PiThread):
 
         return closest_ball_points # (Relative, Absolute) coordinates of closest ball
 
-    def track_ball(self, now: float) -> tuple[bool, bool]:
+    def track_ball(self, now: float) -> bool:
         """
         Runs ball-tracking logic.
         Args:
@@ -209,7 +237,7 @@ class ControllerThread(PiThread):
         """
         # This should never happen ----
         if self._ball_points is None or self._pid_controller is None:
-            return True, False
+            return True
         # -----------------------------
 
         # Update ball position
@@ -218,8 +246,7 @@ class ControllerThread(PiThread):
         if closest_ball_points is None:
             # Go SEARCH if no ball detected for a while
             if now - self._last_detection_time > self.NO_DETECTION_TIMEOUT:
-                return True, False
-            return False, False
+                return True
         else:
             # Check if current detection timed out
             update_position = now - self._last_retarget_time > self.BALL_MAX_LIFESPAN
@@ -234,4 +261,4 @@ class ControllerThread(PiThread):
             # Always refresh detection time if ball visible
             self._last_detection_time = now
             
-            return False, True # Detections are fine, no need for state switch
+        return False # Detections are fine, no need for state switch
